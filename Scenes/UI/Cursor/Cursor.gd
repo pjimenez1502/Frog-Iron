66ebc2dc78612e9151ed83768076e3ab0ec5119b
pjimenez1502 <pjimenez1502@gmail.com>
2025-10-23 00:00:18 +0200

    Custom cursor

---
+++ Scenes/UI/Cursor/Cursor.gd
@@ -0,0 +1,8 @@
+extends Control
+class_name Cursor
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _process(delta: float) -> void:
+	global_position = get_global_mouse_position()
